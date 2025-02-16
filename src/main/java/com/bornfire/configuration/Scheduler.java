3a7f86b3d062d49d6652d73bb07fbc554959e720
Gowtham908 <gowtham.r@bornfire.co.in>
2025-02-16 12:10:05 +0530

    update schedular

--- src/main/java/com/bornfire/configuration/Scheduler.java
+++ src/main/java/com/bornfire/configuration/Scheduler.java
@@ -3,112 +3,118 @@ package com.bornfire.configuration;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import com.bornfire.controllers.IPSRestController;
 import com.bornfire.entity.BipsSwiftMsgConversionRepo;
 
 @Component
 public class Scheduler {
 
-	@Autowired
-	Environment env;
+    @Autowired
+    Environment env;
 
-	@Autowired
-	IPSRestController iPSRestController;
+    @Autowired
+    IPSRestController iPSRestController;
 
-	@Autowired
-	BipsSwiftMsgConversionRepo bipsSwiftMsgConversionRepo;
+    @Autowired
+    BipsSwiftMsgConversionRepo bipsSwiftMsgConversionRepo;
 
-	@Scheduled(fixedRate = 5000)
-	public void run() throws IOException, ParseException {
+    @Scheduled(fixedRate = 5000)
+    public void run() throws IOException, ParseException {
+        processMTOutDirectory();
+        processMXInDirectory();
+    }
 
-		String usr = "Auto";
-		File directoryPath = new File(env.getProperty("auto.swift.mt.out.file.path"));
+    private void processMTOutDirectory() throws IOException, ParseException {
+        String usr = "Auto";
+        File directoryPath = new File(env.getProperty("auto.swift.mt.out.file.path"));
 
-		File filesList[] = directoryPath.listFiles();
-		// System.out.println("List of files :");
-		if (filesList.length != 0) {
-			for (File file : filesList) {
+        if (directoryPath.exists() && directoryPath.isDirectory()) {
+            File[] filesList = directoryPath.listFiles();
 
-				System.out.println("File name: " + file.getName());
-				String filename = file.getName();
-				//
-				//
-				//
-				// MT to MX
-				//
-				//
-				//
+            if (filesList != null) {
+                boolean hasValidFiles = false;
 
-				String msg = iPSRestController.AutoFilepicker(usr, filename);
-				System.out.println(msg);
-				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd_MM_yyyy_HH_mm_ss");
-				LocalDateTime now = LocalDateTime.now();
-				String date = dtf.format(now);
+                for (File file : filesList) {
+                    if (!file.getName().endsWith(".DONE")) {
+                        hasValidFiles = true;
+                        break;
+                    }
+                }
 
-				String filename2 = "Completed_MT" + date + ".IN";
-				// file.renameTo(new File("E:\\MessageConversion\\Out\\" +
-				// file.getName()+file.lastModified()));
-				// file.renameTo((new File("E:\\MessageConversion\\Out\\newFile1.txt"+Count)));
-				file.renameTo(new File(env.getProperty("after.conv.mt.file.output") + filename2));
-				// file.delete();
-				System.out.println("delete" + date);
-				System.out.println(" ");
-			}
-		} else {
-			System.out.println("No Files Present in this source");
+                if (hasValidFiles) {
+                    for (File file : filesList) {
+                        if (file.getName().endsWith(".DONE")) {
+                            continue;
+                        }
 
-		}
+                        System.out.println("Processing File: " + file.getName());
+                        String filename = file.getName();
 
-////////////////////////////////////////////////////////////////////////////////////// MX to MT//////////////////////////////////////////////////////////////////
+                        // MT to MX Processing
+                        String msg = iPSRestController.AutoFilepicker(usr, filename);
+                        System.out.println(msg);
 
-		File directoryPath2 = new File(env.getProperty("auto.swift.mx.in.file.path"));
-		usr = "Auto_MUS";
+                        // Rename the processed file
+                        File renamedFile = new File(file.getParent(), filename + ".DONE");
+                        if (file.renameTo(renamedFile)) {
+                            System.out.println("File renamed to: " + renamedFile.getName());
+                        }
+                    }
+                } else {
+                    System.out.println("No Files Present in this source");
+                }
+            }
+        } else {
+            System.out.println("Invalid MT Out source directory: " + env.getProperty("auto.swift.mt.out.file.path"));
+        }
+    }
 
-		File filesList1[] = directoryPath2.listFiles();
-		System.out.println(filesList1.length);
-		// System.out.println("List of files :");
+    private void processMXInDirectory() throws IOException, ParseException {
+        String usr = "Auto_MUS";
+        File directoryPath = new File(env.getProperty("auto.swift.mx.in.file.path"));
 
-		if (filesList1.length != 0) {
-			for (File file1 : filesList1) {
+        if (directoryPath.exists() && directoryPath.isDirectory()) {
+            File[] filesList = directoryPath.listFiles();
 
-				System.out.println("File name: " + file1.getName());
-				String filename2 = file1.getName();
-				String msg2 = iPSRestController.AutoFilepicker2(usr, filename2);
-				//
-				//
-				//
-				// MX to MT
-				//
-				//
-				//
-				System.out.println(msg2);
-				DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd_MM_yyyy_HH_mm_ss");
-				LocalDateTime now = LocalDateTime.now();
-				String date = dtf.format(now);
+            if (filesList != null) {
+                boolean hasValidFiles = false;
 
-				String filename22 = "Completed_MX" + date + ".OUT";
-				// file.renameTo(new File("E:\\MessageConversion\\Out\\" +
-				// file.getName()+file.lastModified()));
-				// file.renameTo((new File("E:\\MessageConversion\\Out\\newFile1.txt"+Count)));
-				// file1.delete();
-				file1.renameTo(new File(env.getProperty("after.conv.mx.file.output") + filename22));
-				System.out.println("delete");
-				System.out.println(" ");
+                for (File file : filesList) {
+                    if (!file.getName().endsWith(".DONE")) {
+                        hasValidFiles = true;
+                        break;
+                    }
+                }
 
-			}
-		} else {
-			System.out.println("No Files Present in this source2");
-		}
+                if (hasValidFiles) {
+                    for (File file : filesList) {
+                        if (file.getName().endsWith(".DONE")) {
+                            continue;
+                        }
 
-	}
+                        System.out.println("Processing File: " + file.getName());
+                        String filename = file.getName();
+
+                        // MX to MT Processing
+                        String msg2 = iPSRestController.AutoFilepicker2(usr, filename);
+                        System.out.println(msg2);
+
+                        // Rename the processed file
+                        File renamedFile = new File(file.getParent(), filename + ".DONE");
+                        if (file.renameTo(renamedFile)) {
+                            System.out.println("File renamed to: " + renamedFile.getName());
+                        }
+                    }
+                } else {
+                    System.out.println("No Files Present in this source2");
+                }
+            }
+        } else {
+            System.out.println("Invalid MX In source directory: " + env.getProperty("auto.swift.mx.in.file.path"));
+        }
+    }
 }
